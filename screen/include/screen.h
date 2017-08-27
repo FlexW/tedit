0ef55cdf66eef3801925e18ad387772c69025619
Felix Weilbach <feweilbach@gmail.com>
2017-08-27 17:13:41 +0200

    Added screen dir structure

---
+++ screen/include/screen.h
@@ -0,0 +1,4 @@
+#ifndef SCREEN_H_
+#define SCREEN_H_
+
+#endif
---
+++ screen/unit-tests/screen_test.cpp
@@ -0,0 +1,10 @@
+#include "screen/include/screen.h"
+#include "gtest/gtest.h"
+
+class ScreenTest : public ::testing::Test {
+public:
+    ScreenTest() {}
+    ~ScreenTest() {}
+};
+
+TEST_F(ScreenTest, blbla) {}
