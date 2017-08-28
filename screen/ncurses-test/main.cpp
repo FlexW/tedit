e422c7e1aa12c0926ba65fd50587da7cd341e538
Felix Weilbach <feweilbach@gmail.com>
2017-08-28 20:48:32 +0200

    Changed set_size set_pos

--- screen/ncurses-test/main.cpp
+++ screen/ncurses-test/main.cpp
@@ -9,8 +9,8 @@ int main(int argc, char *argv[])
     screen scr;
 
     scr.init();
-    int x = scr.get_max_x();
-    int y = scr.get_max_y();
+    int x = scr.get_max_x() - 2;
+    int y = scr.get_max_y() - 2;
 
     int winx = x / 2;
     auto win1 = scr.new_window(0, 0, winx, y);
@@ -25,15 +25,15 @@ int main(int argc, char *argv[])
 
     scr.set_size(win1, winx / 2, y / 2);
     scr.add_string(win1, 4, 4, "hello");
-    scr.draw(win1);
     scr.draw_border(win1, 0, 0);
+    scr.draw(win1);
 
     std::this_thread::sleep_for(std::chrono::seconds(3));
 
     scr.set_pos(win1, 4, 4);
     scr.add_string(win1, 3, 3, "hello");
-    scr.draw(win1);
     scr.draw_border(win1, 0, 0);
+    scr.draw(win1);
 
     std::this_thread::sleep_for(std::chrono::seconds(3));
 
