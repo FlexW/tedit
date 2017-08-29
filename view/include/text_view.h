461edf660fd5584f06b3abbccf47529641862f0f
Felix Weilbach <feweilbach@gmail.com>
2017-08-29 12:10:30 +0200

    extended text view

--- view/include/text_view.h
+++ view/include/text_view.h
@@ -19,14 +19,44 @@ public:
     class view_changed : public event<int> {
     };
 
+    /**
+     * Opens a textfile and loads it into the text view.
+     * @param filename Name of the file to open.
+     * @returns True on succes. False on failure.
+     */
+    bool open(std::string& filename);
+
+    /**
+     * Sets the textfield.
+     * @param text Text.
+     */
+    void set_text(const std::string& text);
+
 protected:
+    /** Represents one row in the text view. */
     struct row {
-        std::string *text = nullptr; /**< Real text */
-        std::string *rtext = nullptr; /**< Text that gets displayed */
+        std::shared_ptr<std::string> text; /**< Real text */
+        std::shared_ptr<std::string> rtext; /**< Text that gets displayed */
     };
-    std::vector<row*> rows; /**< Holds all rows for the view. */
+
+    std::vector<std::shared_ptr<row>> rows; /**< Holds all rows for the view. */
     int rowoff = 0; /**< Tracks the row user is currently scrolled to. */
     int coloff = 0; /**< Tracks the column user is currently scrolled to. */
+
+    /**
+     * Reads one line from a stream in a string.
+     * @param is Stream to read from.
+     * @param t String to save the line.
+     * @returns Stream. If Strem end then nullptr.
+     */
+    std::istream& get_line(std::istream& is, std::string& t);
+
+    /**
+     * Creates a new row from string.
+     * @param line Text.
+     * @returns Row.
+     */
+    std::shared_ptr<row> new_row(std::string& line);
 };
 
 #endif
